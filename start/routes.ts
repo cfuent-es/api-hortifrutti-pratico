17ed213a6a986305421c24f1c247b320bcc817bc
Cesar Fuentes <cesar@fuentes.net.br>
2022-12-27 13:57:36 -0300

    aula9

--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,34 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Route from '@ioc:Adonis/Core/Route'
+import User from 'App/Models/User'
 
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+Route.get('/create-admin', async () => {
+  User.create({
+    email: 'admin@admin.com',
+    password: 'admin',
+    type: 'admin'
+  })
+})
+
+Route.post('/gettoken', async ({ request, response, auth }: HttpContextContract) => {
+  const email = request.input('email')
+  const password = request.input('password')
+
+  const user = await User.find('email', email)
+
+  if(user == null) {
+    return response.notFound("User not found")
+  }
+
+  const token = await auth.use('api').attempt(email, password)
+
+  return response.ok(token);
+})
+
+Route.get('/auth', async ({ response, auth }: HttpContextContract) => {
+  response.ok("somente usuários autenticados podem ver essa mensagem")
+}).middleware('auth')
